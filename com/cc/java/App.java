8cfd9483d8dc5587c5410d75fb8c3a4a2055565d
RoyStrassberger <rdf3001@googlemail.com>
2022-02-04 11:01:27 +0100

    Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,7 +5,23 @@ public class App {
 
     public static void main(String[] args) {
       
-  
+        // Queen queen = new Queen(); 
+        // Drone drone = new Drone();
+        // Worker worker = new Worker();
+
+        HoneyBee queen = new Queen(); 
+        HoneyBee drone = new Drone();
+        HoneyBee worker = new Worker();
+
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+        output(worker.doYourJob());
+
+        output("------------------------------------");
+
+        output(queen.fly());
+        output(drone.fly());
+        output(worker.fly());
   
     }
 
